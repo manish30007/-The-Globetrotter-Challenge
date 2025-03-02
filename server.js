a52b1e3ce17944e6afc496359be2c2ecfd5a8939
Manish <manish@culturro.com>
2025-03-02 12:54:51 +0530

    fixes for deployement on vercel

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use('/api',require('./Backend/services/data.service'));
 app.use('/game',require('./Backend/controller/game.controller'));
 app.use('/user',require('./Backend/controller/user.controller'));
 
-
+app.get("/", function(req, res){
+    res.send("Your server is up and running");
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
